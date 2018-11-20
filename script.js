826948a954c125188f3412792d0642e0b3c5cf36
vagrant <mikesnow444@gmail.com>
2018-11-20 16:32:23 +0000

    generates y axis labels based on range of inputed values

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const drawBarChart = function(data, options, element){
+var drawBarChart = function(data, options, element){
   //create bar chart outline located at element parameter
-  let barChart = $(element);
-  let table = $("<table class='barChart'></table>");
+  var barChart = $(element);
+  var table = $("<table class='barChart'></table>");
   table.appendTo(barChart);
+  //make a variable that will contain an array of the y-values and be used to determine y increment
+  var yArray = [];
+  //make a variable that will contain the increment the y axis will contain
+  var yAxisInc = 1;
+
+  //fill yArray with y values inputed by user
+  for(let yValue in data){
+    yArray.push(data[yValue]);
+  }
+  //sort yArray and make a loop to determine if increment should be increased based on array range
+  yArray.sort(function(a, b){
+    return a - b;
+  });
+  while(yArray[yArray.length - 1] - yArray[0] > yAxisInc * 5){
+    yAxisInc *= 5;
+  }
+  //make variable that will be displayed on y axis
+  let yAxis = Math.ceil((yArray[yArray.length - 1] / yAxisInc))* yAxisInc;
+
+  // console.log(yAxis);
+  // console.log(yAxisInc);
+  // console.log(yArray);
 
   //loop through data values to generate cells for each bar
-  for(let property in data){
+  while(yAxis > 0){
     let newRow = $("<tr></tr>");
     newRow.appendTo(table);
-    let yLabel = $("<th class='yLabels'>" + data[property] + "</th>");
+    let yLabel = $("<th class='yLabels'>" + yAxis + "</th>");
     yLabel.appendTo(newRow);
+    yAxis -= yAxisInc;
     //make a loop to fill out table cells for each data catagory
     for(let property2 in data){
       let cell = $("<th class='data'</th>");
